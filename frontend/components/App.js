7a559a73fa15405d74047a7d601ad1c212db23ba
Victor Brambila <victorbrambila22@gmail.com>
2022-03-08 23:40:17 -0800

    finished tasks

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,11 +1,96 @@
-import React from 'react'
+import React from 'react';
+import Form from './Form';
+
+let idx = 0
+const getIdx = () => ++idx
+
+const list = [
+  { id: getIdx(), task: 'work on car', completed: false }
+]
+
+const initialState = {
+  list,
+  todo: {
+    todoInput: ''
+  }
+};
 
 export default class App extends React.Component {
+  state = initialState;
+
+  addTodo = () => {
+    const { list, todo: { todoInput } } = this.state
+    const newTask = { id: getIdx(), task: todoInput, completed: false }
+    this.setState({
+      ...this.state,
+      todo: initialState.todo,
+      list: [...list, newTask],
+    })
+  }
+
+  destroy = id => {
+    console.log('clicking to destroy the following id!', id)
+    this.setState({
+      ...this.state, list: this.state.list.filter(item => item.id !== id)
+    })
+  }
+
+  checkComplete = id => {
+    this.setState({
+      ...this.state, list: this.state.list.map(item => {
+        return item.id === id
+        ? { ...item, completed: !item.completed }
+        : item
+      })
+    })
+  }
+
+  changeInput = (key, value) => {
+    this.setState({ 
+      ...this.state, 
+      todo: { ...this.state.todo, [key]: value }
+    })
+  }
+  
   render() {
+    const { list, todo } = this.state;
+
     return (
       <div>
-        Todo App
+        Todo List:
+        <ul>
+          {
+            list.map(item => {
+              const { id, task, completed } = item
+              return (
+                <li key={id}>
+                  {task}
+                  <button onClick={() => this.checkComplete(id)} >{ completed ? 'complete! good job' : 'not complete' }</button>
+                  <button onClick={() => this.destroy(id)}>remove</button>
+                </li>
+              )
+            })
+          }
+        </ul>
+        <Form onChange={this.changeInput} onSubmit={this.addTodo} values={todo} />
       </div>
     )
   }
 }
+
+// destroy = id => {
+//   console.log('clicking to destroy the following id!', id)
+//   this.setState({
+//     ...this.state, quotes: this.state.quotes.filter(quote => quote.id !== id)
+//   })
+// }
+
+// capitalizeAuthor = id => {
+//   this.setState({
+//     ...this.state, quotes: this.state.quotes.map(quote => {
+//       return quote.id === id
+//         ? { ...quote, author: quote.author.toUpperCase() }
+//         : quote
+//     })
+//   })
+// }
--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,10 +1,31 @@
 import React from 'react'
 
 export default class Form extends React.Component {
+  onSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit();
+  }
+  
+  onChange = e => {
+    const { value, id } = e.target;
+    this.props.onChange(id, value);
+  }
+
   render() {
+    const { values } = this.props;
+
     return (
       <div>
-        Form
+        <form onSubmit={this.onSubmit}>
+          <input 
+            type="text"
+            value={values.todoInput}
+            onChange={this.onChange}
+            placeholder="add todo"
+            id="todoInput"
+          />
+          <input type="submit" />
+        </form>
       </div>
     )
   }
